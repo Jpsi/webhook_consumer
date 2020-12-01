5e5f994b89b0aebadb4011cf295623c1d7d59e89
Philip Fol <philip.fol08@gmail.com>
2020-12-01 23:52:47 +0000

    feat: Add route and controller action placeholder for handling webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :transactions, only: [:index] do
+    post 'default_webhook', :on => :collection
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
